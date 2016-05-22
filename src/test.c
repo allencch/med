a437e170734858dfdceb40fc966177976d9aabd6
Allen <allencch@hotmail.com>
2016-05-22 15:23:17 +0800

    Use CMake; file structure re-organise

---
+++ src/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+	int a = 0;
+	while(1) {
+		printf("%d\t",a);
+		printf("a = 0x%x\n",a);
+		int temp;
+		scanf("%d",&temp);
+		if(temp == 0) {
+			continue;
+		}
+		else {
+			a = temp;
+		}
+		printf("%d\t",a);
+		printf("a changed 0x%x\n",a);
+	}
+	return 0;
+}
