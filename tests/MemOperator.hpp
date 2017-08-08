6dff1fe6dfcd97ce4b3905b51369ec8987baaade
Allen <allencch@hotmail.com>
2017-08-09 00:51:58 +0800

    Split the test files

---
+++ tests/MemOperator.hpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <cxxtest/TestSuite.h>
+
+#include "med/MemOperator.hpp"
+
+using namespace std;
+
+class TestMemOperator : public CxxTest::TestSuite {
+public:
+  void testMemGt() {
+    unsigned char ptr1[] = { 0x20, 0x00, 0x00 };
+    unsigned char ptr2[] = { 0x10, 0x00, 0x00 };
+
+    TS_ASSERT_EQUALS(memGt(ptr1, ptr2, sizeof(ptr1)), true);
+  }
+  void testMemGe() {
+    unsigned char ptr1[] = { 0x20, 0x00, 0x00 };
+    unsigned char ptr2[] = { 0x20, 0x00, 0x00 };
+
+    TS_ASSERT_EQUALS(memGe(ptr1, ptr2, sizeof(ptr1)), true);
+
+    ptr2[0] = 0x21;
+    TS_ASSERT_EQUALS(memGe(ptr1, ptr2, sizeof(ptr1)), false);
+
+    // Test endianness
+    ptr1[0] = 0x00;
+    ptr1[1] = 0x01;
+    TS_ASSERT_EQUALS(memGe(ptr1, ptr2, sizeof(ptr1)), true);
+  }
+  void testMemWithin() {
+    unsigned char src[] = { 0x20, 0x00, 0x00 };
+    unsigned char low[] = { 0x10, 0x00, 0x00 };
+    unsigned char up[] = { 0x30, 0x00, 0x00 };
+    TS_ASSERT_EQUALS(memWithin(src, low, up, sizeof(src)), true);
+
+    up[0] = 0x00;
+    up[2] = 0xff;
+    TS_ASSERT_EQUALS(memWithin(src, low, up, sizeof(src)), true);
+  }
+
+  void testMemCompare() {
+    unsigned char src[] = { 0x20, 0x00, 0x00 };
+    unsigned char low[] = { 0x10, 0x00, 0x00 };
+    unsigned char up[] = { 0x30, 0x00, 0x00 };
+    TS_ASSERT_EQUALS(memCompare(src, low, sizeof(src), ScanParser::Ge), true);
+    TS_ASSERT_EQUALS(memCompare(src, up, sizeof(src), ScanParser::Ge), false);
+  }
+
+  void testMemCompareWithin() {
+    unsigned char src[] = { 0x20, 0x00 };
+    unsigned char dest[] = { 0x10, 0x00, 0x21, 0x00 };
+    TS_ASSERT_EQUALS(memCompare(src, sizeof(src),
+                                dest, sizeof(dest),
+                                ScanParser::Within), true);
+
+    dest[2] = 0x19;
+    TS_ASSERT_EQUALS(memCompare(src, sizeof(src),
+                                dest, sizeof(dest),
+                                ScanParser::Within), false);
+  }
+};
--- tests/ScanParser.hpp
+++ tests/ScanParser.hpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <cxxtest/TestSuite.h>
 
-#include "med/MemOperator.hpp"
 #include "med/ScanParser.hpp"
 
 using namespace std;
@@ -33,7 +32,7 @@ class TestScanParser : public CxxTest::TestSuite {
     s = "<=1234";
     op = ScanParser::getOp(s);
     TS_ASSERT_EQUALS(op, "<=");
-    
+
     s = "<1234";
     op = ScanParser::getOp(s);
     TS_ASSERT_EQUALS(op, "<");
@@ -79,60 +78,5 @@ class TestScanParser : public CxxTest::TestSuite {
     values = ScanParser::getValues(s);
     TS_ASSERT_EQUALS(values.size(), 1);
     TS_ASSERT_EQUALS(values[0], "10");
-  }
-};
-
-class TestMemOperator : public CxxTest::TestSuite {
-public:
-  void testMemGt() {
-    unsigned char ptr1[] = { 0x20, 0x00, 0x00 };
-    unsigned char ptr2[] = { 0x10, 0x00, 0x00 };
-    
-    TS_ASSERT_EQUALS(memGt(ptr1, ptr2, sizeof(ptr1)), true);
-  }
-  void testMemGe() {
-    unsigned char ptr1[] = { 0x20, 0x00, 0x00 };
-    unsigned char ptr2[] = { 0x20, 0x00, 0x00 };
-    
-    TS_ASSERT_EQUALS(memGe(ptr1, ptr2, sizeof(ptr1)), true);
-
-    ptr2[0] = 0x21;
-    TS_ASSERT_EQUALS(memGe(ptr1, ptr2, sizeof(ptr1)), false);
-
-    // Test endianness
-    ptr1[0] = 0x00;
-    ptr1[1] = 0x01;
-    TS_ASSERT_EQUALS(memGe(ptr1, ptr2, sizeof(ptr1)), true);
-  }
-  void testMemWithin() {
-    unsigned char src[] = { 0x20, 0x00, 0x00 };
-    unsigned char low[] = { 0x10, 0x00, 0x00 };
-    unsigned char up[] = { 0x30, 0x00, 0x00 };
-    TS_ASSERT_EQUALS(memWithin(src, low, up, sizeof(src)), true);
-
-    up[0] = 0x00;
-    up[2] = 0xff;
-    TS_ASSERT_EQUALS(memWithin(src, low, up, sizeof(src)), true);
-  }
-
-  void testMemCompare() {
-    unsigned char src[] = { 0x20, 0x00, 0x00 };
-    unsigned char low[] = { 0x10, 0x00, 0x00 };
-    unsigned char up[] = { 0x30, 0x00, 0x00 };
-    TS_ASSERT_EQUALS(memCompare(src, low, sizeof(src), ScanParser::Ge), true);
-    TS_ASSERT_EQUALS(memCompare(src, up, sizeof(src), ScanParser::Ge), false);
-  }
-
-  void testMemCompareWithin() {
-    unsigned char src[] = { 0x20, 0x00 };
-    unsigned char dest[] = { 0x10, 0x00, 0x21, 0x00 };
-    TS_ASSERT_EQUALS(memCompare(src, sizeof(src),
-                                dest, sizeof(dest),
-                                ScanParser::Within), true);
-
-    dest[2] = 0x19;
-    TS_ASSERT_EQUALS(memCompare(src, sizeof(src),
-                                dest, sizeof(dest),
-                                ScanParser::Within), false);
   }
 };
