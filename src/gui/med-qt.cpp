b5475d2408a257c6a9cf17f48b91a7e659452b92
Allen <allencch@hotmail.com>
2017-12-20 21:38:48 +0800

    Segment fault tracing

--- src/gui/med-qt.cpp
+++ src/gui/med-qt.cpp
@@ -8,6 +8,11 @@
 #include <QtUiTools>
 #include <QtDebug>
 
+// For segment fault tracing
+#include <execinfo.h>
+#include <unistd.h>
+#include <signal.h>
+
 #include "gui/med-qt.hpp"
 #include "gui/TreeItem.hpp"
 #include "gui/TreeModel.hpp"
@@ -20,6 +25,18 @@
 #include "med/med.hpp"
 
 using namespace std;
+
+// https://stackoverflow.com/questions/77005/how-to-automatically-generate-a-stacktrace-when-my-gcc-c-program-crashes
+void handler(int sig) {
+  void* array[10];
+  size_t size;
+
+  size = backtrace(array, 10);
+
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
 
 MainUi::MainUi(QApplication* app) {
   this->app = app;
@@ -746,6 +763,7 @@ void MainUi::clearAll() {
 /////////////////////
 
 int main(int argc, char **argv) {
+  signal(SIGSEGV, handler);
   QApplication app(argc, argv);
   new MainUi(&app);
   return app.exec();
