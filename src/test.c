c076b2f3ae82af8a18081473787f8f38e770a1ba
Allen <allencch@hotmail.com>
2019-10-17 23:36:58 +0800

    Spacing

--- src/test.c
+++ src/test.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 int main() {
   int a = 0;
-  while(1) {
-    printf("%d\t",a);
-    printf("a = 0x%x\n",a);
+  while (1) {
+    printf("%d\t", a);
+    printf("a = 0x%x\n", a);
     int temp;
-    scanf("%d",&temp);
-    if(temp == 0) {
+    scanf("%d", &temp);
+    if (temp == 0) {
       continue;
-    }
-    else {
+    } else {
       a = temp;
     }
-    printf("%d\t",a);
-    printf("a changed 0x%x\n",a);
+    printf("%d\t", a);
+    printf("a changed 0x%x\n", a);
   }
   return 0;
 }
