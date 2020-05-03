6276492112a7aa056eca1670f88cc3bf0d32753e
Allen <allencch@hotmail.com>
2020-05-03 13:34:41 +0800

    Get ScanCommand

--- include/med/ScanCommand.hpp
+++ include/med/ScanCommand.hpp
@@ -2,13 +2,14 @@
 #define SCAN_COMMAND_HPP
 
 #include <vector>
+#include <string>
 #include "med/SubCommand.hpp"
 
 using namespace std;
 
 class ScanCommand {
 public:
-  ScanCommand();
+  explicit ScanCommand(const string& s);
   vector<SubCommand> getSubCommands();
 private:
   vector<SubCommand> subCommands;
--- src/med/ScanCommand.cpp
+++ src/med/ScanCommand.cpp
@@ -1,6 +1,15 @@
 #include "med/ScanCommand.hpp"
+#include "med/ScanParser.hpp"
 
-ScanCommand::ScanCommand() {}
+ScanCommand::ScanCommand(const string& s) {
+  auto values = ScanParser::getValues(s);
+
+  for (size_t i = 0; i < values.size(); i++) {
+    auto value = values[i];
+    SubCommand subCmd(value);
+    subCommands.push_back(subCmd);
+  }
+}
 
 vector<SubCommand> ScanCommand::getSubCommands() {
   return subCommands;
